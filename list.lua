4c6b10e944c513f0ba368e861d063a03c7462340
flerex <94745162+shutupue@users.noreply.github.com>
2025-01-30 20:21:21 +0000

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,7 +1,11 @@
 return {
     {
       id = 127072670296905,
-      name = "Skibidi Toilet Poop",
+      name = "Skibidi Toilet Block",
+    },
+     {
+      id = 94816904033653,
+      name = "Fat Run",
     }
   }
   
